6d2c4b69fd9368686468a4bdcaae4119b11d7542
leejiwon6315 <leejiwon6315@naver.com>
2020-07-31 11:53:41 +0900

    update

--- movie_app_clone_coding/src/App.js
+++ movie_app_clone_coding/src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie"; 
+import "./App.css";
+import MyHeader from "./MyHeader";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    isLoading : true,
+    movies: []
+  };
+  
+getMovies = async () => {
+  const {
+    data: {
+      data: { movies }
+    }
+  } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+  
+  this.setState({ movies, isLoading: false });
+};
+
+async componentDidMount(){
+  // mount 과정에서 render() 실행 이후, 호출되는 함수
+
+  this.getMovies();
+};
+
+  render(){
+
+    const { isLoading, movies } = this.state;
+    return(
+      <section className = "container">
+        { isLoading 
+        ? <div className = "loader">
+            <span className = "loader_text">Loading</span>
+          </div> 
+        : <>
+          <MyHeader/>
+          <div className = "movies">
+              {
+                movies.map(movie => (
+
+                  <Movie
+                    key={movie.id}
+                    id={movie.id}
+                    year={movie.year} 
+                    title={movie.title} 
+                    genres={movie.genres}
+                    summary={movie.summary} 
+                    poster={movie.medium_cover_image} 
+                  />
+                )) 
+              }
+            </div>
+
+            </>
+        }  
+      </section>
+      
+    );
+  }
+
 }
 
 export default App;
---
+++ movie_app_clone_coding/src/MyHeader.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./MyHeader.css";
+
+function MyHeader() {
+    return <div class = "header">
+        <h1 class = "header_title"> My Movie List From API </h1>
+    </div>;
+};
+
+export default MyHeader;
